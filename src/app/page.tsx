d49a69fb19d87327c935debf450db73c3f1e869e
chandan-16 <chandangautam420.cg@gmail.com>
2025-07-10 15:57:19 +0530

    Added the signup sign in feature of clerk in the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import "./globals.css";
+import Header from "@/components/Header";
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
 export default function Home() {
   return (
-  <>
-  
-  </>
+  <main className={inter.className}>
+  <Header />
+  </main>
   );
 }
